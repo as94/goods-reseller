6f670f46b3f611ebedacccfd8994979a9149e562
Anatoly Sorokin <a.sorokin@dodopizza.com>
2021-01-03 22:48:56 +0300

    add version to contracts

--- GoodsReseller.Domain/DataCatalog/Prices/PriceContract.cs
+++ GoodsReseller.Domain/DataCatalog/Prices/PriceContract.cs
@@ -5,6 +5,7 @@ namespace GoodsReseller.Domain.DataCatalog.Prices
     public class PriceContract
     {
         public Guid PriceId { get; set; }
+        public int Version { get; set; }
         public Guid ProductId { get; set; }
         public decimal UnitPrice { get; set; }
         public int CurrencyId { get; set; }
