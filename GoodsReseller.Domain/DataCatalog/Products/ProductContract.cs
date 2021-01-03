b3989df301109cbdf5a7cc19b2c43f425fee1986
Anatoly Sorokin <anatoly.sorokin.personal@gmail.com>
2021-01-03 22:48:56 +0300

    add version to contracts

--- GoodsReseller.Domain/DataCatalog/Products/ProductContract.cs
+++ GoodsReseller.Domain/DataCatalog/Products/ProductContract.cs
@@ -5,6 +5,7 @@ namespace GoodsReseller.Domain.DataCatalog.Products
     public class ProductContract
     {
         public Guid ProductId { get; set; }
+        public int Version { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
     }
