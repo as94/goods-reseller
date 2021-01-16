a77b0de3f5fe0e964cdf6b8757c3f5216d3fc6e5
Anatoly Sorokin <a.sorokin@dodopizza.com>
2021-01-16 23:12:09 +0300

    pass API_URL env

--- app/webpack.common.ts
+++ app/webpack.common.ts
@@ -1,6 +1,15 @@
 import { CheckerPlugin } from 'awesome-typescript-loader'
-import { Configuration } from 'webpack'
+import { Configuration, DefinePlugin } from 'webpack'
 import CopyPlugin from 'copy-webpack-plugin'
+import dotenv from 'dotenv'
+import fs from 'fs'
+import path from 'path'
+
+const currentPath = path.join(__dirname)
+const basePath = currentPath + '/.env'
+const envPath = basePath + '.' + process.env.ENVIRONMENT
+const finalPath = fs.existsSync(envPath) ? envPath : basePath
+const env = dotenv.config({ path: finalPath }).parsed
 
 const commonConfig: Configuration = {
 	entry: ['./src/index.tsx'],
@@ -51,6 +60,12 @@ const commonConfig: Configuration = {
 			{ from: 'public/assets', to: __dirname + '/../src/GoodsReseller.Api/wwwroot/assets' },
 			{ from: 'public/favicon.ico', to: __dirname + '/../src/GoodsReseller.Api/wwwroot/favicon.ico' },
 		]),
+		new DefinePlugin(
+			Object.keys(env).reduce((prev, next) => {
+				prev[`process.env.${next}`] = JSON.stringify(env[next])
+				return prev
+			}, {}),
+		),
 	],
 }
 
