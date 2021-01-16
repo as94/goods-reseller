a8dde3bd494647aeceb17cec0526adf0d9c295f9
Anatoly Sorokin <a.sorokin@dodopizza.com>
2021-01-16 18:15:06 +0300

    add CookiePolicyOptions

--- src/GoodsReseller.Api/Startup.cs
+++ src/GoodsReseller.Api/Startup.cs
@@ -8,6 +8,8 @@ using GoodsReseller.OrderContext.Handlers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.CookiePolicy;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -17,10 +19,12 @@ namespace GoodsReseller.Api
     public class Startup
     {
         private readonly IConfiguration _configuration;
+        private readonly IHostEnvironment _environment;
 
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IHostEnvironment environment)
         {
             _configuration = configuration;
+            _environment = environment;
         }
 
         // This method gets called by the runtime. Use this method to add services to the container.
@@ -28,6 +32,14 @@ namespace GoodsReseller.Api
         {
             var goodsResellerDatabaseSection = _configuration.GetSection(nameof(GoodsResellerDatabaseOptions));
             services.Configure<GoodsResellerDatabaseOptions>(goodsResellerDatabaseSection);
+            
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                options.MinimumSameSitePolicy = SameSiteMode.Strict;
+                options.HttpOnly = HttpOnlyPolicy.None;
+                options.Secure = _environment.IsDevelopment()
+                    ? CookieSecurePolicy.None : CookieSecurePolicy.Always;
+            });
             
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
                 .AddCookie(options =>
