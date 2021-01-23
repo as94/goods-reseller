98f754b090abaa93d7c6bdee9470e83a768bc957
Anatoly Sorokin <a.sorokin@dodopizza.com>
2021-01-23 15:22:16 +0300

    add <Dashboard />

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import Login from './Login/Login'
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom'
-import Home from './Home/Home'
 import Register from './Register/Register'
 import { AuthProvider } from './Hooks/useAuth'
 import PrivateRoute from './PrivateRoute'
+import Dashboard from './Dashboard/Dashboard'
 
 const App = () => (
 	<AuthProvider>
 		<Router>
 			<Switch>
 				<PrivateRoute exact path={['/', '/admin']}>
-					<Home />
+					<Dashboard />
 				</PrivateRoute>
 				<Route path="/admin/login">
 					<Login />
