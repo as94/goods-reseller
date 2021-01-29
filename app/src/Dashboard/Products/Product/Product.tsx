b1c1471b670b89ff6bb341dc39abc04e899af741
Anatoly Sorokin <anatoly.sorokin.personal@gmail.com>
2021-01-30 00:01:18 +0300

    implement edit product

--- app/src/Dashboard/Products/Product/Product.tsx
+++ app/src/Dashboard/Products/Product/Product.tsx
@@ -1,9 +1,11 @@
 import { Box, Button, FormControl, FormHelperText, Grid, Input, InputLabel, TextField } from '@material-ui/core'
 import React, { useCallback, useEffect, useState } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import { ProductContract } from '../../../Api/Products/contracts'
+import { ProductContract, ProductInfoContract } from '../../../Api/Products/contracts'
 import productsApi from '../../../Api/Products/productsApi'
 import Title from '../../Title'
+import { formIsValid, FormValidation, initialFormValidation, initialProduct } from '../utils'
+import { Alert } from '@material-ui/lab'
 
 interface IOwnProps {
 	productId: string | null
@@ -36,71 +38,152 @@ const Product = ({ productId, hide }: IOwnProps) => {
 
 	const classes = useStyles()
 
-	const [product, setProduct] = useState(null as ProductContract | null)
+	const [product, setProduct] = useState(initialProduct as ProductInfoContract)
+	const [formValidation, setFormValidation] = useState(initialFormValidation(true) as FormValidation)
+	const [errorText, setErrorText] = useState('')
+
+	const backHandler = useCallback(() => hide(), [hide])
+
+	const labelChangeHandler = useCallback(
+		(e: any) => {
+			const label = e.target.value
+			setProduct({ ...product, label })
+			setFormValidation({ ...formValidation, labelValid: label })
+		},
+		[product, setProduct, formValidation, setFormValidation],
+	)
+
+	const nameChangeHandler = useCallback(
+		(e: any) => {
+			const name = e.target.value
+			setProduct({ ...product, name })
+			setFormValidation({ ...formValidation, nameValid: name })
+		},
+		[product, setProduct, formValidation, setFormValidation],
+	)
+
+	const descriptionChangeHandler = useCallback(
+		(e: any) => {
+			setProduct({ ...product, description: e.target.value })
+		},
+		[product, setProduct],
+	)
+
+	const unitPriceChangeHandler = useCallback(
+		(e: any) => {
+			const unitPrice = Number(e.target.value)
+			setProduct({ ...product, unitPrice })
+			setFormValidation({ ...formValidation, unitPriceValid: unitPrice >= 0 })
+		},
+		[product, setProduct, formValidation, setFormValidation],
+	)
+
+	const discountPerUnitChangeHandler = useCallback(
+		(e: any) => {
+			const discountPerUnit = Number(e.target.value)
+			setProduct({ ...product, discountPerUnit })
+			setFormValidation({ ...formValidation, discountPerUnitValid: discountPerUnit >= 0 && discountPerUnit <= 1 })
+		},
+		[product, setProduct, formValidation, setFormValidation],
+	)
 
 	const getProduct = useCallback(async () => {
 		const response = await productsApi.GetProduct(productId)
-		setProduct(response)
+		setProduct({
+			name: response.name,
+			label: response.label,
+			description: response.description,
+			unitPrice: response.unitPrice,
+			discountPerUnit: response.discountPerUnit,
+			productIds: response.products ? response.products.map(p => p.id) : [],
+		} as ProductInfoContract)
 	}, [setProduct, productId])
+
+	const updateProduct = useCallback(async () => {
+		if (formIsValid(formValidation)) {
+			await productsApi.Update(productId, product)
+			hide()
+		} else {
+			setErrorText('Form is invalid')
+		}
+	}, [formIsValid, formValidation, product, productsApi, hide, setErrorText])
 
 	useEffect(() => {
 		getProduct()
 	}, [getProduct])
 
-	const backHandler = useCallback(() => hide(), [hide])
+	useEffect(() => {
+		if (formIsValid(formValidation)) {
+			setErrorText('')
+		}
+	}, [formValidation, formIsValid, setErrorText])
 
 	return (
-		product && (
-			<React.Fragment>
-				<Box pt={2}>
-					<Title>Edit product</Title>
-				</Box>
-
-				<Grid container spacing={3}>
-					<Grid item xs={12} md={12}>
-						<FormControl fullWidth>
-							<InputLabel htmlFor="name">Name</InputLabel>
-							<Input id="name" value={product.name} />
-						</FormControl>
-					</Grid>
-					<Grid item xs={12} md={12}>
-						<FormControl fullWidth>
-							<InputLabel htmlFor="label">Label</InputLabel>
-							<Input id="label" value={product.label} />
-						</FormControl>
-					</Grid>
-					<Grid item xs={12} md={12}>
-						<FormControl fullWidth>
-							<InputLabel htmlFor="description">Description</InputLabel>
-							<Input id="description" value={product.description} />
-						</FormControl>
-					</Grid>
-					<Grid item xs={12} md={12}>
-						<FormControl fullWidth>
-							<InputLabel htmlFor="unitPrice">Unit Price</InputLabel>
-							<Input required type="number" id="unitPrice" value={product.unitPrice} />
-						</FormControl>
-					</Grid>
-					<Grid item xs={12} md={12}>
-						<FormControl fullWidth>
-							<InputLabel htmlFor="discountPerUnit">Discount Per Unit</InputLabel>
-							<Input required type="number" id="discountPerUnit" value={product.discountPerUnit} />
-						</FormControl>
-					</Grid>
+		<React.Fragment>
+			<Box pt={2}>
+				<Title>Edit product</Title>
+			</Box>
+			<Grid container spacing={3}>
+				<Grid item xs={12} md={12}>
+					<FormControl error={!formValidation.nameValid} fullWidth>
+						<InputLabel htmlFor="name">Name</InputLabel>
+						<Input id="name" value={product.name} onChange={nameChangeHandler} />
+					</FormControl>
 				</Grid>
-				<div className={classes.buttons}>
-					<Button variant="contained" onClick={() => null} className={classes.removeButton}>
-						Remove
-					</Button>
-					<Button onClick={backHandler} className={classes.button}>
-						Back
-					</Button>
-					<Button variant="contained" color="primary" onClick={() => null} className={classes.button}>
-						Save
-					</Button>
-				</div>
-			</React.Fragment>
-		)
+				<Grid item xs={12} md={12}>
+					<FormControl error={!formValidation.labelValid} fullWidth>
+						<InputLabel htmlFor="label">Label</InputLabel>
+						<Input id="label" value={product.label} onChange={labelChangeHandler} />
+					</FormControl>
+				</Grid>
+				<Grid item xs={12} md={12}>
+					<FormControl fullWidth>
+						<InputLabel htmlFor="description">Description</InputLabel>
+						<Input id="description" value={product.description} onChange={descriptionChangeHandler} />
+					</FormControl>
+				</Grid>
+				<Grid item xs={12} md={12}>
+					<FormControl error={!formValidation.unitPriceValid} fullWidth>
+						<InputLabel htmlFor="unitPrice">Unit Price</InputLabel>
+						<Input
+							required
+							type="number"
+							id="unitPrice"
+							value={product.unitPrice}
+							onChange={unitPriceChangeHandler}
+						/>
+					</FormControl>
+				</Grid>
+				<Grid item xs={12} md={12}>
+					<FormControl error={!formValidation.discountPerUnitValid} fullWidth>
+						<InputLabel htmlFor="discountPerUnit">Discount Per Unit</InputLabel>
+						<Input
+							required
+							type="number"
+							id="discountPerUnit"
+							value={product.discountPerUnit}
+							onChange={discountPerUnitChangeHandler}
+						/>
+					</FormControl>
+				</Grid>
+				{errorText && (
+					<Grid item xs={12} md={12}>
+						<Alert severity="error">{errorText}</Alert>
+					</Grid>
+				)}
+			</Grid>
+			<div className={classes.buttons}>
+				<Button variant="contained" onClick={() => null} className={classes.removeButton}>
+					Remove
+				</Button>
+				<Button onClick={backHandler} className={classes.button}>
+					Back
+				</Button>
+				<Button variant="contained" color="primary" onClick={updateProduct} className={classes.button}>
+					Save
+				</Button>
+			</div>
+		</React.Fragment>
 	)
 }
 
