84e8bab7fa602f113104d491059020e0d56f40be
Anatoly Sorokin <anatoly.sorokin.personal@gmail.com>
2021-02-14 00:00:36 +0300

    remove mongo from infrastructure

--- src/GoodsReseller.Infrastructure/InfrastructureRegistry.cs
+++ src/GoodsReseller.Infrastructure/InfrastructureRegistry.cs
@@ -7,24 +7,16 @@ using GoodsReseller.Infrastructure.OrderContext;
 using GoodsReseller.OrderContext.Domain.Orders;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
-using MongoDB.Driver;
 
 namespace GoodsReseller.Infrastructure
 {
     public static class InfrastructureRegistry
     {
         public static void RegisterInfrastructure(this IServiceCollection serviceCollection,
-            GoodsResellerDatabaseOptions goodsResellerDatabaseOptions,
             DatabaseOptions databaseOptions)
         {
             serviceCollection.AddDbContextPool<GoodsResellerDbContext>(options =>
                 options.UseNpgsql(databaseOptions.ConnectionString));
-            
-            var mongoClient = new MongoClient(goodsResellerDatabaseOptions.ConnectionString);
-            var mongoDatabase = mongoClient.GetDatabase(goodsResellerDatabaseOptions.DatabaseName);
-
-            serviceCollection.AddSingleton<IMongoClient>(mongoClient);
-            serviceCollection.AddSingleton(mongoDatabase);
             
             serviceCollection.AddScoped<IProductsRepository, ProductsRepository>();
             serviceCollection.AddScoped<IUsersRepository, UsersRepository>();
