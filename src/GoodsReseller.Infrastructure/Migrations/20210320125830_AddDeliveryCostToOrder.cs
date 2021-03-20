7132c5e003ad1ab21b8bffe0ada17db98588fc5f
Anatoly Sorokin <anatoly.sorokin.personal@gmail.com>
2021-03-20 16:41:35 +0300

    added TODO

---
+++ src/GoodsReseller.Infrastructure/Migrations/20210320125830_AddDeliveryCostToOrder.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace GoodsReseller.Infrastructure.Migrations
+{
+    public partial class AddDeliveryCostToOrder : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "DeliveryCostValue",
+                table: "orders",
+                type: "numeric",
+                nullable: true,
+                defaultValue: 0m);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeliveryCostValue",
+                table: "orders");
+        }
+    }
+}
