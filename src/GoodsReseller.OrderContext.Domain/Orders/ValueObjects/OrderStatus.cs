c250ea8492c650066cf06858a2f4db3a5e1a40b5
Anatoly Sorokin <a.sorokin@dodopizza.com>
2021-05-16 13:21:48 +0300

    removed redundant order statuses

--- src/GoodsReseller.OrderContext.Domain/Orders/ValueObjects/OrderStatus.cs
+++ src/GoodsReseller.OrderContext.Domain/Orders/ValueObjects/OrderStatus.cs
@@ -8,12 +8,9 @@ namespace GoodsReseller.OrderContext.Domain.Orders.ValueObjects
         {
         }
         
-        public static readonly OrderStatus DataReceived = new OrderStatus(1, "DataReceived");
-        public static readonly OrderStatus Accepted = new OrderStatus(2, "Accepted");
-        public static readonly OrderStatus Collected = new OrderStatus(3, "Collected");
-        public static readonly OrderStatus Packed = new OrderStatus(4, "Packed");
-        public static readonly OrderStatus Shipped = new OrderStatus(5, "Shipped");
-        public static readonly OrderStatus Completed = new OrderStatus(6, "Completed");
-        public static readonly OrderStatus Canceled = new OrderStatus(7, "Canceled");
+        public static readonly OrderStatus Accepted = new OrderStatus(1, "Accepted");
+        public static readonly OrderStatus Packed = new OrderStatus(2, "Packed");
+        public static readonly OrderStatus Shipped = new OrderStatus(3, "Shipped");
+        public static readonly OrderStatus Canceled = new OrderStatus(4, "Canceled");
     }
 }
