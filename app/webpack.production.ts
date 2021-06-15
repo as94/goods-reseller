4d24d1a86caa50a95cae3b68e3225ba71f34ada0
Anatoly Sorokin <anatoly.sorokin.personal@gmail.com>
2021-06-15 21:43:14 +0300

    optimized prod webpack

--- app/webpack.production.ts
+++ app/webpack.production.ts
@@ -2,8 +2,18 @@ import merge from 'webpack-merge'
 import commonConfig from './webpack.common'
 import { Configuration } from 'webpack-dev-server'
 
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+
 const productionConfig: Configuration = merge(commonConfig, {
+	devtool: false,
 	mode: 'production',
+	plugins: [
+		new UglifyJsPlugin({
+			cache: true,
+			parallel: true,
+			sourceMap: true
+		})
+	]
 })
 
 export default productionConfig
