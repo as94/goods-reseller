76b6b4311986f093be90eecf3a27e5c3c546c1e7
Anatoly Sorokin <anatoly.sorokin.personal@gmail.com>
2021-07-31 22:10:35 +0300

    add deliveryType checking fix bugs

--- app/src/Store/Note/Note.tsx
+++ app/src/Store/Note/Note.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import Container from '@material-ui/core/Container/Container'
-import Typography from '@material-ui/core/Typography/Typography'
+import Container from '@material-ui/core/Container'
+import Typography from '@material-ui/core/Typography'
 
 const useStyles = makeStyles(theme => ({
 	cardGrid: {
