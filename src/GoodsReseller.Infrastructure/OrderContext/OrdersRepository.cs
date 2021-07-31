bd40d968e10485269f4ab328e373a51946e05963
Anatoly Sorokin <a.sorokin@dodopizza.com>
2021-07-31 20:38:57 +0300

    refactoring order adding

--- src/GoodsReseller.Infrastructure/OrderContext/OrdersRepository.cs
+++ src/GoodsReseller.Infrastructure/OrderContext/OrdersRepository.cs
@@ -61,86 +61,7 @@ namespace GoodsReseller.Infrastructure.OrderContext
                     await _dbContext.Database.BeginTransactionAsync(IsolationLevel.RepeatableRead, cancellationToken);
                 try
                 {
-                    foreach (var orderItem in order.OrderItems)
-                    {
-                        await _dbContext.Database.ExecuteSqlRawAsync(
-                            @"
-                                update order_items set
-                                ""ProductId"" = @productId,
-                                ""UnitPriceValue"" = @unitPriceValue,
-                                ""QuantityValue"" = @quantityValue,
-                                ""DiscountPerUnitValue"" = @discountPerUnitValue,
-                                ""CreationDate"" = @creationDate,
-                                ""CreationDateUtc"" = @creationDateUtc,
-                                ""IsRemoved"" = @isRemoved,
-                                ""LastUpdateDate"" = @lastUpdateDate,
-                                ""LastUpdateDateUtc"" = @lastUpdateDateUtc
-                                where ""Id"" = @id;
-                            ",
-                            new []
-                            {
-                                new NpgsqlParameter("@id", orderItem.Id),
-                                new NpgsqlParameter("@productId", orderItem.ProductId),
-                                new NpgsqlParameter("@unitPriceValue", orderItem.UnitPrice.Value),
-                                new NpgsqlParameter("@quantityValue", orderItem.Quantity.Value),
-                                new NpgsqlParameter("@discountPerUnitValue", orderItem.DiscountPerUnit.Value),
-                                new NpgsqlParameter("@creationDate", orderItem.CreationDate.Date),
-                                new NpgsqlParameter("@creationDateUtc", orderItem.CreationDate.DateUtc),
-                                new NpgsqlParameter("@isRemoved", orderItem.IsRemoved),
-                                new NpgsqlParameter("@lastUpdateDate",
-                                    orderItem.LastUpdateDate != null
-                                        ? (object)orderItem.LastUpdateDate?.Date
-                                        : DBNull.Value),
-                                new NpgsqlParameter("@lastUpdateDateUtc",
-                                    orderItem.LastUpdateDate != null
-                                        ? (object)orderItem.LastUpdateDate?.DateUtc
-                                        : DBNull.Value),
-                            },
-                            cancellationToken);
-                    }
-                    
-                    var rowsAffected = await _dbContext.Database.ExecuteSqlRawAsync(
-                        @"
-                                update orders set
-                                ""Version"" = @version,
-                                ""Address"" = CAST(@address AS json),
-                                ""CustomerInfo"" = CAST(@customerInfo AS json),
-                                ""TotalCostValue"" = @totalCostValue,
-                                ""Status_Id"" = @statusId,
-                                ""Status_Name"" = @statusName,
-                                ""DeliveryCostValue"" = @deliveryCostValue,
-                                ""AddedCostValue"" = @addedCostValue,
-                                ""CreationDate"" = @creationDate,
-                                ""CreationDateUtc"" = @creationDateUtc,
-                                ""IsRemoved"" = @isRemoved,
-                                ""LastUpdateDate"" = @lastUpdateDate,
-                                ""LastUpdateDateUtc"" = @lastUpdateDateUtc
-                                where ""Id"" = @id and ""Version"" <= @version;
-                            ",
-                        new []
-                        {
-                            new NpgsqlParameter("@id", order.Id),
-                            new NpgsqlParameter("@version", order.Version),
-                            new NpgsqlParameter("@address", JsonSerializer.Serialize(order.Address)),
-                            new NpgsqlParameter("@customerInfo", JsonSerializer.Serialize(order.CustomerInfo)),
-                            new NpgsqlParameter("@totalCostValue", order.TotalCost.Value),
-                            new NpgsqlParameter("@statusId", order.Status.Id),
-                            new NpgsqlParameter("@statusName", order.Status.Name),
-                            new NpgsqlParameter("@deliveryCostValue", order.DeliveryCost.Value),
-                            new NpgsqlParameter("@addedCostValue", order.AddedCost.Value),
-                            new NpgsqlParameter("@creationDate", order.CreationDate.Date),
-                            new NpgsqlParameter("@creationDateUtc", order.CreationDate.DateUtc),
-                            new NpgsqlParameter("@isRemoved", order.IsRemoved),
-                            new NpgsqlParameter("@lastUpdateDate",
-                                order.LastUpdateDate != null
-                                    ? (object)order.LastUpdateDate?.Date
-                                    : DBNull.Value),
-                            new NpgsqlParameter("@lastUpdateDateUtc",
-                                order.LastUpdateDate != null
-                                    ? (object)order.LastUpdateDate?.DateUtc
-                                    : DBNull.Value),
-                        },
-                        cancellationToken);
+                    var rowsAffected = await AddOrderAsync(order, cancellationToken);
 
                     if (rowsAffected == 0)
                     {
@@ -157,15 +78,106 @@ namespace GoodsReseller.Infrastructure.OrderContext
                 }
             }
         }
-        
+
         private async Task<int?> GetVersionAsync(Guid orderId, CancellationToken cancellationToken)
         {
             var orderVersion = await _dbContext.OrderVersions.FromSqlRaw(
-                $@"select ""Version"" from orders where ""Id"" = @orderId",
-                new NpgsqlParameter("@orderId", orderId))
+                    $@"select ""Version"" from orders where ""Id"" = @orderId",
+                    new NpgsqlParameter("@orderId", orderId))
                 .FirstOrDefaultAsync(cancellationToken);
 
             return orderVersion?.Version;
+        }
+
+        private async Task<int> AddOrderAsync(Order order, CancellationToken cancellationToken)
+        {
+            await AddOrderItemsAsync(order, cancellationToken);
+            
+            var rowsAffected = await _dbContext.Database.ExecuteSqlRawAsync(
+                @"
+                                update orders set
+                                ""Version"" = @version,
+                                ""Address"" = CAST(@address AS json),
+                                ""CustomerInfo"" = CAST(@customerInfo AS json),
+                                ""TotalCostValue"" = @totalCostValue,
+                                ""Status_Id"" = @statusId,
+                                ""Status_Name"" = @statusName,
+                                ""DeliveryCostValue"" = @deliveryCostValue,
+                                ""AddedCostValue"" = @addedCostValue,
+                                ""CreationDate"" = @creationDate,
+                                ""CreationDateUtc"" = @creationDateUtc,
+                                ""IsRemoved"" = @isRemoved,
+                                ""LastUpdateDate"" = @lastUpdateDate,
+                                ""LastUpdateDateUtc"" = @lastUpdateDateUtc
+                                where ""Id"" = @id and ""Version"" <= @version;
+                            ",
+                new[]
+                {
+                    new NpgsqlParameter("@id", order.Id),
+                    new NpgsqlParameter("@version", order.Version),
+                    new NpgsqlParameter("@address", JsonSerializer.Serialize(order.Address)),
+                    new NpgsqlParameter("@customerInfo", JsonSerializer.Serialize(order.CustomerInfo)),
+                    new NpgsqlParameter("@totalCostValue", order.TotalCost.Value),
+                    new NpgsqlParameter("@statusId", order.Status.Id),
+                    new NpgsqlParameter("@statusName", order.Status.Name),
+                    new NpgsqlParameter("@deliveryCostValue", order.DeliveryCost.Value),
+                    new NpgsqlParameter("@addedCostValue", order.AddedCost.Value),
+                    new NpgsqlParameter("@creationDate", order.CreationDate.Date),
+                    new NpgsqlParameter("@creationDateUtc", order.CreationDate.DateUtc),
+                    new NpgsqlParameter("@isRemoved", order.IsRemoved),
+                    new NpgsqlParameter("@lastUpdateDate",
+                        order.LastUpdateDate != null
+                            ? (object) order.LastUpdateDate?.Date
+                            : DBNull.Value),
+                    new NpgsqlParameter("@lastUpdateDateUtc",
+                        order.LastUpdateDate != null
+                            ? (object) order.LastUpdateDate?.DateUtc
+                            : DBNull.Value),
+                },
+                cancellationToken);
+            
+            return rowsAffected;
+        }
+
+        private async Task AddOrderItemsAsync(Order order, CancellationToken cancellationToken)
+        {
+            foreach (var orderItem in order.OrderItems)
+            {
+                await _dbContext.Database.ExecuteSqlRawAsync(
+                    @"
+                                update order_items set
+                                ""ProductId"" = @productId,
+                                ""UnitPriceValue"" = @unitPriceValue,
+                                ""QuantityValue"" = @quantityValue,
+                                ""DiscountPerUnitValue"" = @discountPerUnitValue,
+                                ""CreationDate"" = @creationDate,
+                                ""CreationDateUtc"" = @creationDateUtc,
+                                ""IsRemoved"" = @isRemoved,
+                                ""LastUpdateDate"" = @lastUpdateDate,
+                                ""LastUpdateDateUtc"" = @lastUpdateDateUtc
+                                where ""Id"" = @id;
+                            ",
+                    new[]
+                    {
+                        new NpgsqlParameter("@id", orderItem.Id),
+                        new NpgsqlParameter("@productId", orderItem.ProductId),
+                        new NpgsqlParameter("@unitPriceValue", orderItem.UnitPrice.Value),
+                        new NpgsqlParameter("@quantityValue", orderItem.Quantity.Value),
+                        new NpgsqlParameter("@discountPerUnitValue", orderItem.DiscountPerUnit.Value),
+                        new NpgsqlParameter("@creationDate", orderItem.CreationDate.Date),
+                        new NpgsqlParameter("@creationDateUtc", orderItem.CreationDate.DateUtc),
+                        new NpgsqlParameter("@isRemoved", orderItem.IsRemoved),
+                        new NpgsqlParameter("@lastUpdateDate",
+                            orderItem.LastUpdateDate != null
+                                ? (object) orderItem.LastUpdateDate?.Date
+                                : DBNull.Value),
+                        new NpgsqlParameter("@lastUpdateDateUtc",
+                            orderItem.LastUpdateDate != null
+                                ? (object) orderItem.LastUpdateDate?.DateUtc
+                                : DBNull.Value),
+                    },
+                    cancellationToken);
+            }
         }
     }
 }
