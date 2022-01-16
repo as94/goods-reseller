eaa1c9c409a48c22a9f65ff93b670c5d929fa848
Anatoly Sorokin <anatoly.sorokin.personal@gmail.com>
2022-01-16 16:39:48 +0200

    extract tests to folder

--- tests/GoodsReseller.IntegrationTests/Infrastructure/GoodsResellerResult.cs
+++ tests/GoodsReseller.IntegrationTests/Infrastructure/GoodsResellerResult.cs
@@ -5,13 +5,13 @@ namespace GoodsReseller.IntegrationTests.Infrastructure
     public class GoodsResellerResult
     {
         public HttpStatusCode StatusCode { get; set; }
-        public string? Error { get; set; }
+        public string Error { get; set; }
     }
     
     public class GoodsResellerResult<T>
     {
         public HttpStatusCode StatusCode { get; set; }
         public T Data { get; set; }
-        public string? Error { get; set; }
+        public string Error { get; set; }
     }
 }
